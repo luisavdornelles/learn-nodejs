fa065fdcbded4aa059ce95c2e74e0333a9c86ff3
Luisa Vargas <luisavdornelles@gmail.com>
2024-12-17 16:32:59 -0500

    Using csv-parse to convert the chunk of data into objects

--- planets-project/index.js
+++ planets-project/index.js
@@ -4,6 +4,10 @@ const fs = require("fs");
 const results = [];
 
 fs.createReadStream("kepler_data.csv")
+    .pipe(parse({ // parse function will run with the chunk ready from the createReadStream
+        comment: "#", // consider # as comments
+        columns: true, // each line will be an object, not an array
+    }))
     .on("data", (data) => {
         results.push(data);
     })
