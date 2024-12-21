c9d3159e9cd5c08fea30ccc32b7ed452e2525c92
Luisa Vargas <luisavdornelles@gmail.com>
2024-12-21 13:54:14 -0500

    Testing out parameterized URLs

--- http-server/index.js
+++ http-server/index.js
@@ -6,16 +6,35 @@ const PORT = 3000;
 // res is a writable stream
 const server = http.createServer();
 
+const friends = [
+    {
+      id: 0,
+      name: 'Nikola Tesla',
+    },
+    {
+      id: 1,
+      name: 'Sir Isaac Newton',
+    },
+    {
+      id: 2,
+      name: 'Albert Einstein',
+    }
+];
+
 server.on("request", (req, res) => {
-    if (req.url === "/friends") {
-        res.writeHead(200, {
-            "Content-Type": "application/json",
-        });
-        res.end(JSON.stringify({
-            id: 1,
-            name: "luisa",
-        }));
-    } else if (req.url === '/messages') {
+    const items = req.url.split('/');
+    // /friends/2 => ['', 'friends', '2']
+    // /friends/
+    if (items[1] === "friends") {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        if (items.length === 3) {
+        const friendIndex = Number(items[2]);
+        res.end(JSON.stringify(friends[friendIndex]));
+        } else {
+        res.end(JSON.stringify(friends));
+        }
+    } else if (items[1] === 'messages') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
