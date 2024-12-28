171cd489ee7574532568591c10151a02d8977c6e
Luisa Vargas <luisavdornelles@gmail.com>
2024-12-28 13:14:51 -0500

    Testing different routes

--- express-project/server.js
+++ express-project/server.js
@@ -6,6 +6,18 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.send("helloooo");
+});
+
+app.get("/messages", (req, res) => {
+    res.send("<ul><li>helloooo</li></ul>");
+});
+
+app.post("/messages", (req, res) => {
+    res.send("Updating messages...");
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
 });
