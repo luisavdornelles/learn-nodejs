5dfe0440c11cc79e451a485264d4595edc3382cb
Luisa Vargas <luisavdornelles@gmail.com>
2025-03-09 13:58:41 -0400

    Serving a file

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,5 +1,7 @@
+const path = require("path");
+
 function getMessages(req, res) {
-    res.send("<ul><li>helloooo</li></ul>");
+    res.sendFile(path.join(__dirname, "..", "public", "skimountain.jpg"));
 };
 
 function postMessages(req, res) {
