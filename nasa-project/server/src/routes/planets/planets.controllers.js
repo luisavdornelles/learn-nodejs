5f143fa0c8ff7bebfdf00e77541f77e595cc3b7f
Luisa Vargas <luisavdornelles@gmail.com>
2025-03-26 17:56:17 -0400

    Setting up GET /planets

---
+++ nasa-project/server/src/routes/planets/planets.controllers.js
@@ -0,0 +1,11 @@
+const planets = require("../../models/planets.model");
+
+function getAllPlanets(req, res) {
+    // using return to avoid setting the status twice by mistake
+    // also helps with readability
+    return res.status(200).json(planets);
+}
+
+module.exports = {
+    getAllPlanets,
+}
