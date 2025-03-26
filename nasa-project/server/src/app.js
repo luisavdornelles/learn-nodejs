195b04e20aaa559a102ccd9d093c6d1c7756c71a
Luisa Vargas <luisavdornelles@gmail.com>
2025-03-26 18:01:01 -0400

    CORS header allowlist

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
+
+// allow client-side server to make requests to the API
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
 
 app.use(express.json());
 app.use(planetsRouter);
