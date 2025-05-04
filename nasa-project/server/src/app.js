d444f8c55a25363db659fb57181c89eb272166d3
Luisa Vargas <luisavdornelles@gmail.com>
2025-05-04 17:16:02 -0400

    Adding Morgan dependency

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -2,17 +2,23 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
+const morgan = require("morgan");
 
 const app = express();
 
 // allow client-side server to make requests to the API
-app.use(cors({
-    origin: "http://localhost:3000"
-}));
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    })
+);
+
+// loggin middleware should go as high up as possible
+app.use(morgan("combined"));
 
 // middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "public")))
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(planetsRouter);
 
